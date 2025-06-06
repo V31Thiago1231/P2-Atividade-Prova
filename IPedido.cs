ceb1b058754338a19b4fe1c0595a1d30ee8e248e
V31Thiago1231 <thiago.malheiro@sou.unifeb.edu.br>
2025-06-05 22:47:27 -0300

    Criando agora a in0terface Pedido

---
+++ IPedido.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace LojaVirtual
+{
+    public interface IPedido
+    {
+        int Id { get; set; }
+        ICliente Cliente { get; set; }
+        List<ItemPedido> Itens { get; set; }
+        DateTime Data { get; set; }
+        decimal ValorTotal { get; }
+        void AdicionarItem(IProduto produto, int quantidade);
+        void RemoverItem(int produtoId);
+        void CalcularValorTotal();
+    }
+} 
--- Pedido.cs
+++ Pedido.cs
@@ -4,8 +4,51 @@ using System.Linq;
 
 namespace LojaVirtual
 {
-    public class Pedido 
+    public class Pedido : IPedido
     {
+        public int Id { get; set; }
+        public ICliente Cliente { get; set; }
+        public List<ItemPedido> Itens { get; set; }
+        public DateTime Data { get; set; }
+        public decimal ValorTotal { get; private set; }
 
+        public Pedido()
+        {
+            Itens = new List<ItemPedido>();
+            Data = DateTime.Now;
+        }
+
+        public void AdicionarItem(IProduto produto, int quantidade)
+        {
+            if (quantidade <= 0)
+                throw new ArgumentException("A quantidade deve ser maior que zero.");
+
+            var itemExistente = Itens.FirstOrDefault(i => i.Produto.Id == produto.Id);
+            if (itemExistente != null)
+            {
+                itemExistente.Quantidade += quantidade;
+            }
+            else
+            {
+                Itens.Add(new ItemPedido { Produto = produto, Quantidade = quantidade });
+            }
+
+            CalcularValorTotal();
+        }
+
+        public void RemoverItem(int produtoId)
+        {
+            var item = Itens.FirstOrDefault(i => i.Produto.Id == produtoId);
+            if (item != null)
+            {
+                Itens.Remove(item);
+                CalcularValorTotal();
+            }
+        }
+
+        public void CalcularValorTotal()
+        {
+            ValorTotal = Itens.Sum(item => item.Subtotal);
+        }
     }
 } 
